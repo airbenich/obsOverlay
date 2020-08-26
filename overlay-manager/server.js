dec2fcbcee1f7d8ad67e5c90552fc0b604c0e2cb
andeedotnet <andee@andee.net>
2020-08-26 13:25:38 +0200

    api

--- overlay-manager/server.js
+++ overlay-manager/server.js
@@ -16,8 +16,63 @@ nunjucks.configure('views', {
     express: app
 });
 
+// TODO persistant storage 
+var overlays = []
+
+overlays.push ({ 
+    id: 0,
+    name: "test",
+    subtitle: "testsubtitle"
+});
+
+var displays = []
+
+displays.push ({ 
+    id: 0,
+    name: "OBS"
+});
+
+// APP ROUTES
 app.get('/', (req, res) => {
     res.render('index.html');
+})
+
+// API ROUTES
+app.get('/api/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ status: "ok" }));
+})
+
+// OVERLAY API
+
+// get overlay list
+app.get('/api/overlays/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(overlays));
+})
+
+// create overlay
+app.put('/api/overlay/:overlayId', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ status: "ok" }));
+})
+
+// edit overlay
+app.post('/api/overlay/:overlayId', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ status: "ok" }));
+})
+
+// delete overlay
+app.delete('/api/overlay/:overlayId', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ status: "ok" }));
+})
+
+// VIEW API
+app.get('/api/displays/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(displays));
 })
 
 app.listen(port, '0.0.0.0', () => {
