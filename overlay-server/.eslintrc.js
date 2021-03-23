4265ac57af0774f83b11b320f3d45147d4981147
Ruben Klepp <ruben.klepp@stud.h-da.de>
2021-03-23 23:24:54 +0100

    Initial setup

---
+++ overlay-server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': 'off',
+  },
+};
