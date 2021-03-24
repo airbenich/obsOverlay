e98f8cc27c388a57f57731db85cb5651e016835c
Vivian Erbenich <vivian@erbenich.eu>
2021-03-24 02:17:03 +0100

    Adds CORS header to overlay-server for local testing

--- overlay-server/src/app.ts
+++ overlay-server/src/app.ts
@@ -3,7 +3,14 @@ import config from './config.json';
 
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
 app.get('/', (req: any, res: any) => {
   res.sendFile(`${__dirname}/index.html`);
