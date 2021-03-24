828adb66be592c7695c2da3d7342466a2fc8d3ab
Ruben Klepp <ruben.klepp@stud.h-da.de>
2021-03-24 02:25:01 +0100

    Fix style issues

--- overlay-server/build/app.js
+++ overlay-server/build/app.js
@@ -7,7 +7,14 @@ const LowerthirdsManager_1 = require("./LowerthirdsManager");
 const config_json_1 = __importDefault(require("./config.json"));
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:4200',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['my-custom-header'],
+        credentials: true,
+    },
+});
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
--- overlay-server/src/app.ts
+++ overlay-server/src/app.ts
@@ -5,11 +5,11 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:4200",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
-  }
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
 });
 
 app.get('/', (req: any, res: any) => {
