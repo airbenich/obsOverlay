77d30a990b13c327e751b7754740bf6a65ab0314
andeedotnet <andee@andee.net>
2021-04-28 15:18:00 +0200

    settings wip, moved cleanup button to  settings

--- overlay-manager/src/app/components/header/header.component.ts
+++ overlay-manager/src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import isElectron from 'is-electron';
-import { ModalService } from 'src/app/shared/components/modal';
 import { OverlayServerService } from 'src/app/shared/services/overlay-server/overlay-server.service';
 
 @Component({
@@ -12,22 +11,8 @@ export class HeaderComponent implements OnInit {
   iselectron = false;
   constructor(
     public overlayServerService: OverlayServerService,
-    private modalService: ModalService
   ) {
     this.iselectron = isElectron();
-  }
-
-  openModal(id: string) {
-    this.modalService.open(id);
-  }
-
-  public onUserConfirmedOberlayCleanUp(id: string): void {
-    this.overlayServerService.cleanUpLowerThirds();
-    this.closeModal(id);
-  }
-
-  closeModal(id: string) {
-    this.modalService.close(id);
   }
 
   ngOnInit(): void {}
--- overlay-manager/src/app/components/settings/settings.component.ts
+++ overlay-manager/src/app/components/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import isElectron from 'is-electron';
+import { ModalService } from 'src/app/shared/components/modal';
+import { OverlayServerService } from 'src/app/shared/services/overlay-server/overlay-server.service';
 
 @Component({
   selector: 'app-settings',
@@ -9,8 +12,27 @@ export class SettingsComponent implements OnInit {
   overlayServerHost = "localhost"
   overlayServerPort = 0
   overlayServerAuthCode = ""
+  iselectron = false;
 
-  constructor() { }
+  constructor(
+    public overlayServerService: OverlayServerService,
+    private modalService: ModalService
+  ) {
+    this.iselectron = isElectron();
+  }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  public onUserConfirmedOberlayCleanUp(id: string): void {
+    this.overlayServerService.cleanUpLowerThirds();
+    this.closeModal(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
 
   ngOnInit(): void {
   }
