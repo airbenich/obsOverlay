ed706246d9371cc2ed7514f0d1fd6aa80d0d7c12
airbenich <vivian@erbenich.eu>
2021-05-03 18:28:02 +0200

    move status component to header

--- overlay-manager/src/app/app.module.ts
+++ overlay-manager/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { OverlayListComponent } from './components/home/overlay-list/overlay-list.component';
-import { StatusComponent } from './components/status/status.component';
+import { StatusComponent } from './components/header/status/status.component';
 import { OverlayEditComponent } from './components/home/overlay-edit/overlay-edit.component';
 import { ButtonTileComponent } from './shared/components/button-tile/button-tile.component';
 import { HeaderComponent } from './components/header/header.component';
